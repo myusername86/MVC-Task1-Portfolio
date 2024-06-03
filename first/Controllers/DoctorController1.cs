9a8dff2339dbaf6d520730a5792edaa4fbcbbb58
yuva <pyuvarani.2011@gmail.com>
2024-06-03 15:42:56 +0200

    added Temperature link

---
+++ first/Controllers/DoctorController1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace first.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor/CheckTemperature
+        [HttpGet]
+        public IActionResult CheckTemperature()
+        {
+            return View();
+        }
+
+        // POST: Doctor/CheckTemperature
+        [HttpPost]
+        public IActionResult CheckTemperature(double? temperature, string scale)
+        {
+            if (temperature == null)
+            {
+                ViewBag.Message = "Please enter a temperature value.";
+            }
+            else
+            {
+                double tempCelsius = scale == "Fahrenheit" ? (temperature.Value - 32) * 5 / 9 : temperature.Value;
+                if (tempCelsius >= 38)
+                {
+                    ViewBag.Message = "You have a fever.";
+                }
+                else if (tempCelsius < 35)
+                {
+                    ViewBag.Message = "You have hypothermia.";
+                }
+                else
+                {
+                    ViewBag.Message = "Your temperature is normal.";
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ first/Models/TemperatureCheck.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace first.Models
+{
+    public class TemperatureCheck
+    {
+        [Required]
+        [Display(Name = "Temperature (°F)")]
+        public double Temperature { get; set; }
+
+        public string Message { get; set; }
+
+        public void CheckTemperature()
+        {
+            if (Temperature >= 100.4)
+            {
+                Message = "You have a fever.";
+            }
+            else if (Temperature < 90)
+            {
+                Message = "You have hypothermia.";
+            }
+            else
+            {
+                Message = "Your temperature is normal.";
+            }
+        }
+    }
+}
+//using System.ComponentModel.DataAnnotations;
+
+//namespace first.Models
+//{
+//    public class TemperatureCheck
+//    {
+//        [Required(ErrorMessage = "Temperature is required.")]
+//        public double? Temperature { get; set; }
+
+//        [Required(ErrorMessage = "Scale is required.")]
+//        public required string Scale { get; set; }
+
+//        public required string ResultMessage { get; set; }
+
+//        public void EvaluateTemperature()
+//        {
+//            if (Temperature == null)
+//            {
+//                ResultMessage = "Please enter a temperature value.";
+//                return;
+//            }
+
+//            double tempCelsius = Scale == "Fahrenheit" ? (Temperature.Value - 32) * 5 / 9 : Temperature.Value;
+
+//            if (tempCelsius >= 38)
+//            {
+//                ResultMessage = "You have a fever.";
+//            }
+//            else if (tempCelsius < 35)
+//            {
+//                ResultMessage = "You have hypothermia.";
+//            }
+//            else
+//            {
+//                ResultMessage = "Your temperature is normal.";
+//            }
+//        }
+//    }
+//}
